d5640236bdc0550fbaca33a589ee54408235e2ed
lohakaneaneri6@gmail.com <lohakaneaneri6@gmail.com>
2024-08-11 11:57:20 -0700

    Cursor

---
+++ List/LinkedList/cursor/program1.java
@@ -0,0 +1,28 @@
+package List.LinkedList.cursor;
+
+import java.util.*;
+class IteratorDemo{
+
+    public static void main(String[] args) {
+        
+        ArrayList al = new ArrayList();
+
+        al.add("Kanha");
+        al.add("Rahul");
+        al.add("Ashish");
+
+        Iterator itr = al.iterator();
+
+        while(itr.hasNext()){
+
+           // String currentElement = itr.next();
+
+            if("Rahul".equals(itr.next())){
+            itr.remove();
+            System.out.println(itr.next());
+        }
+    }
+        System.out.println(al);
+    }
+}
+
